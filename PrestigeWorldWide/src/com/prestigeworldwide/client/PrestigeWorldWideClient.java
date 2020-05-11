7acb5152cd02cce1663d03779f59f38361ce1fc4
Cody Baermann <cbaermann@comcast.net>
2020-05-11 08:43:06 -0700

    pushing latest so we can fix bug

--- PrestigeWorldWide/src/com/prestigeworldwide/client/PrestigeWorldWideClient.java
+++ PrestigeWorldWide/src/com/prestigeworldwide/client/PrestigeWorldWideClient.java
@@ -1,13 +1,20 @@
 package com.prestigeworldwide.client;
 
+import com.prestigeworldwide.players.Elf;
+import com.prestigeworldwide.players.Player;
 import com.prestigeworldwide.scanner.PlayerScanner;
 
 
 public class PrestigeWorldWideClient {
     public static void main(String[] args) {
-        PlayerScanner p = new PlayerScanner();
-        p.title();
-        p.chosePlayer();
+//        PlayerScanner p = new PlayerScanner();
+//        p.title();
+//        p.chosePlayer();
+
+        Player e = new Elf();
+        System.out.println(e.toString());
+        e.setNumItems(1);
+        System.out.println(e.toString());
 
 
     }

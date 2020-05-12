e451a240f261e6dfa8928800a9f92eaea02b1242
Cody Baermann <cbaermann@comcast.net>
2020-05-12 09:58:21 -0700

    trying to push

--- PrestigeWorldWide/src/com/prestigeworldwide/players/Dwarf.java
+++ PrestigeWorldWide/src/com/prestigeworldwide/players/Dwarf.java
@@ -10,20 +10,4 @@ public class Dwarf extends Player{
         setStrength(1);
     }
 
-//    @Override
-//    public void playerAttack(Enemy enemy) {
-//        if(enemy.getHealth() > 0) {
-//            System.out.println(getName() + " attacked for " + getStrength() + " damage");
-//            enemy.setHealth(enemy.getHealth() - this.getStrength());
-//        }
-//        if(enemy.getHealth()<= 0){
-//            System.out.println("You defeated " + enemy.getName());
-//        }
-//
-//    }
-//
-//    @Override
-//    public void defend() {
-//
-//    }
 }
